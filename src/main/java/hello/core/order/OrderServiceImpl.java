1a0861f08edb7fa658447f6274b81591825d6684
Dev-Joon <closeplz0526@gmail.com>
2024-09-18 19:02:08 +0900

    스프링 핵심 원리 - 기본편 > 섹션4. 스프링 핵심 원리 이해2 - IoC, DI, 그리고 컨테이너

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,8 +3,6 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class OrderServiceImpl implements OrderService {
 

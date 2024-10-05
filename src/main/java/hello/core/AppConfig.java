08f1feb589598beef2fc8ab9ea3863eb279c4bc0
Dev-Joon <closeplz0526@gmail.com>
2024-10-05 19:04:38 +0900

    스프링 핵심 원리 - 기본편 > 섹션7. 컴포넌트 스캔 > 중복 등록과 충돌

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,10 +14,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    //@Bean memberService -> new MemoryMemberRepository()
-    //@Bean orderService -> new MemoryMemberRepository()
-
-
     @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
